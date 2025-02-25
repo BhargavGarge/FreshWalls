4306baf6f2707bd8feb387bc451de32d4f1526e1
BhargavGarge <bhargaavvv@gmail.com>
2025-02-25 16:35:20 +0530

    Debugging Pending

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -3,11 +3,19 @@ import { Tabs } from "expo-router";
 
 export default function TabLayout() {
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: "blue", headerShown: false }}>
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: "white  n",
+        headerShown: false,
+        tabBarActiveBackgroundColor: "black",
+        tabBarInactiveBackgroundColor: "black",
+      }}
+    >
       <Tabs.Screen
         name="index"
         options={{
           title: "Home",
+
           tabBarIcon: ({ color }) => (
             <FontAwesome size={28} name="home" color={color} />
           ),
--- app/(tabs)/explore.tsx
+++ app/(tabs)/explore.tsx
@@ -1,46 +1,116 @@
-import React, { useState } from "react";
-import {
-  View,
-  Text,
-  SafeAreaView,
-  Image,
-  StyleSheet,
-  FlatList,
-} from "react-native";
-import { Link } from "expo-router";
-import ParallaxScrollView from "@/components/ParallaxScrollView";
-import { useWallpapers, Wallpaper } from "@/hooks/useWallpaper";
-import { ImageCard } from "@/components/ImageCard";
-import { ThemedView } from "@/components/ThemedView";
-import { DownloadPicture } from "@/components/BottomSheet";
+import { StyleSheet, Dimensions, Text, View, Image } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+import { useWallpapers } from "@/hooks/useWallpaper";
 import { SplitView } from "@/components/SplitView";
+import Carousel from "react-native-reanimated-carousel";
+import { useState } from "react";
+import { useCarousel } from "@/hooks/useCarousel";
+import Animated, {
+  interpolate,
+  useAnimatedStyle,
+} from "react-native-reanimated";
+import { LinearGradient } from "expo-linear-gradient";
+import { ThemedSafeAreaView } from "@/components/ThemedSafeAreaView";
 
-export default function Explore() {
+const TOPBAR_HEIGHT = 250;
+
+export default function explore() {
   const wallpapers = useWallpapers();
-  const [selectedWallpaper, setSelectedWallpaper] = useState<null | Wallpaper>(
-    null
-  );
+  const width = Dimensions.get("window").width;
+  const [yOffset, setScrollY] = useState(0);
+  const carouselItems = useCarousel();
+
+  const headerAnimatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          scale: interpolate(
+            yOffset,
+            [-TOPBAR_HEIGHT, 0, TOPBAR_HEIGHT],
+            [1.5, 1, 1]
+          ),
+        },
+      ],
+    };
+  });
+
+  const textAnimatedStyle = useAnimatedStyle(() => {
+    return {
+      opacity: interpolate(
+        yOffset,
+        [-TOPBAR_HEIGHT, TOPBAR_HEIGHT / 2, TOPBAR_HEIGHT],
+        [1, 1, 0]
+      ),
+    };
+  });
+
   return (
-    <SafeAreaView style={styles.container}>
-      <ParallaxScrollView
-        headerBackgroundColor={{
-          dark: "#000",
-          light: "#fff",
-        }}
-        headerImage={
-          <Image
-            source={{
-              uri: wallpapers[0]?.url ?? "",
-            }}
-            style={{
-              flex: 1,
-            }}
-          />
-        }
+    <ThemedSafeAreaView style={{ flex: 1 }}>
+      <Animated.View
+        style={[
+          { height: Math.max(0, TOPBAR_HEIGHT - yOffset) },
+          headerAnimatedStyle,
+        ]}
       >
-        <SplitView wallpapers={wallpapers} />
-      </ParallaxScrollView>
-    </SafeAreaView>
+        <Carousel
+          width={width}
+          data={carouselItems}
+          onSnapToItem={(index) => console.log("current index:", index)}
+          renderItem={({ index }) => (
+            <>
+              <View
+                style={{
+                  flex: 1,
+                  borderWidth: 1,
+                  justifyContent: "center",
+                }}
+              >
+                <Image
+                  source={{ uri: carouselItems[index].image }}
+                  style={{ height: TOPBAR_HEIGHT }}
+                />
+              </View>
+
+              <LinearGradient
+                colors={["transparent", "black"]}
+                style={{
+                  flex: 1,
+                  position: "absolute",
+                  zIndex: 10,
+                  height: TOPBAR_HEIGHT / 2,
+                  width: "100%",
+                  bottom: 0,
+                }}
+              >
+                <Animated.View style={textAnimatedStyle}>
+                  <Text
+                    style={[
+                      {
+                        color: "white",
+                        paddingTop: TOPBAR_HEIGHT / 3,
+                        textAlign: "center",
+                        fontSize: 30,
+                        fontWeight: "600",
+                      },
+                    ]}
+                  >
+                    {carouselItems[index].title}
+                  </Text>
+                </Animated.View>
+              </LinearGradient>
+            </>
+          )}
+        />
+      </Animated.View>
+      <View style={{ borderRadius: 20 }}>
+        <SplitView
+          onScroll={(yOffset) => {
+            setScrollY(yOffset);
+          }}
+          wallpapers={wallpapers}
+        />
+      </View>
+    </ThemedSafeAreaView>
   );
 }
 
